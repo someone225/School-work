4cf06f522fb9512722d2615b93ee952c9c655039
someone225 <marksheng2020@gmail.com>
2025-10-02 17:28:48 -0400

    2025-10-02

--- ENGR133/10/task 3/py4_team_3_19.py
+++ ENGR133/10/task 3/py4_team_3_19.py
@@ -33,13 +33,85 @@ Academic Integrity Statement:
     another student access to my code.  The project I am
     submitting is my own original work.
 """
-
+#imports
 import math as m
-INPUT_FILE_PATH = '"C:\Users\marks\Documents\GitHub\School-work\ENGR133\10\task 3\py4_task3_input.txt"'
+import requests
+
+#global constants
+
 
 
 def main():
-    inputFile = open(INPUT_FILE_PATH)
+    #pull input file data from github repo
+    url = "https://raw.githubusercontent.com/someone225/School-work/refs/heads/main/ENGR133/10/task%203/py4_task3_input.txt"
+    load_repo = requests.get(url)
+    #load input contents into a local file for local access
+    with open("input.txt", "w") as t:
+        t.write(load_repo.text)
+    parsed_input = [''] * 6
+    input_values = [''] * 6
+
+    #open local file for reading
+    input_file = open("input.txt", "r")   
+    
+    for i in range (0, len(parsed_input)): 
+        parsed_input[i] = str(input_file.readlines(1))
+
+        start_index = 0
+        start_reached = False
+
+        end_index = 0
+
+        for j in range(0, len(parsed_input[i])):
+            if(parsed_input[i][start_index] == ':'):
+                start_reached = True
+
+            if(start_reached == False):
+                start_index += 1
+        for j in range (start_index + 1, len(parsed_input[i]) - 4):
+            #upper bound set as len() - 4 as the last 4 characters \n'] must be removed
+            input_values[i] += parsed_input[i][j]
+
+    input_file.close()
+
+    
+    test = substance()
+    test.initialize(input_values)
+    print(test.absobancy[2])
+     
+'''
+substance is a class which contains data about the target substance
+
+basic class calls:
+first declare a class as class_name = substance() in main
+then reference a target value using class_name.value
+eg. to get substance name, use class_name.name
+'''
+
+class substance:
+    name = ''
+    path_length = 0
+    extincion_coefficient = 0
+    absobancy = [0] * 3
+
+    def initialize(self, values):
+        indexer = 0
+        for i in range(0, len(values)):
+            match i:
+                case 0:
+                    self.name = values[i]
+                case 1:
+                    self.path_length = int(values[i])
+                case 2:
+                    self.extincion_coefficient = int(values[i])
+                case _:
+                    self.absobancy[indexer] = float(values[i])
+                    indexer += 1
+        
+            
+    
+
+    
 
 
 if __name__ == "__main__":
--- ENGR133/9/task 3/py3_team_2_b_19.py
+++ ENGR133/9/task 3/py3_team_2_b_19.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 Course Number: ENGR 13300
 Semester: Fall 2025
@@ -55,61 +54,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-"""
-Course Number: ENGR 13300
-Semester: Fall 2025
-
-Description:
-    Creating a loop with the mathematical factorial series.
-
-Assignment Information:
-    Assignment:     9.2.2. Py3 Team 2 B
-    Team ID:        007 - 19
-    Author:         Akshada Dake, dakea@purdue.edu
-    Date:           09/23/2025
-
-Contributors:
-    Name, login@purdue [repeat for each]
-
-    My contributor(s) helped me:
-    [ ] understand the assignment expectations without
-        telling me how they will approach it.
-    [ ] understand different ways to think about a solution
-        without helping me plan my solution.
-    [ ] think through the meaning of a specific error or
-        bug present in my code without looking at my code.
-    Note that if you helped somebody else with their code, you
-    have to list that person as a contributor here as well.
-
-Academic Integrity Statement:
-    I have not used source code obtained from any unauthorized
-    source, either modified or unmodified; nor have I provided
-    another student access to my code.  The project I am
-    submitting is my own original work.
-"""
-
-from math import factorial
-def my_factorial(n: int) -> int:
-    if n<0:
-        return -999 #defined error flag by task B
-    if n==0:
-        return 1 #because mathematically 0! =1
-    result=1
-    i=1 
-    while i<=n: #loop begins
-        result *= i #multiplying the result with i
-        i += 1 #incrementing i
-    return result
-
-def main():
-    n=input("Enter a number: ")
-    result=my_factorial(int(n))
-    if result==-999:
-        print("Error -999 [Negative input].")
-    else:
-        print(f"The Factorial of {n} is {result}.")
-
-if __name__ == "__main__":
-    main()
->>>>>>> a998878671f29f8f2a90f144218016037d78c5a9
