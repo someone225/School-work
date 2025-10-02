de86808038c3ba08cf1721c0a75072599328ae68
someone225 <marksheng2020@gmail.com>
2025-10-02 11:51:58 -0400

    b

---
+++ ENGR133/10/task 3/py4_team_3_19.py
@@ -0,0 +1,46 @@
+"""
+Course Number: ENGR 13300
+Semester: Fall 2025
+
+Description:
+    calculate photon absorbtion of a material given an input file containint relevant data
+
+Assignment Information:
+    Assignment:     e.g. 10.2.3 Py4 Team 19 
+    Team ID:        007 - 19
+    Author:         mark, sheng65@purdue.edu
+    Date:           e.g. 01/23/2025
+
+Contributors:
+    Mark, sheng65@purdue
+    Akshada, dakea@purdue
+    Erdem, eamarsa@purdue
+    Milagros, mmelhemb@purdue 
+
+    My contributor(s) helped me:
+    [ ] understand the assignment expectations without
+        telling me how they will approach it.
+    [ ] understand different ways to think about a solution
+        without helping me plan my solution.
+    [ ] think through the meaning of a specific error or
+        bug present in my code without looking at my code.
+    Note that if you helped somebody else with their code, you
+    have to list that person as a contributor here as well.
+
+Academic Integrity Statement:
+    I have not used source code obtained from any unauthorized
+    source, either modified or unmodified; nor have I provided
+    another student access to my code.  The project I am
+    submitting is my own original work.
+"""
+
+import math as m
+INPUT_FILE_PATH = '"C:\Users\marks\Documents\GitHub\School-work\ENGR133\10\task 3\py4_task3_input.txt"'
+
+
+def main():
+    inputFile = open(INPUT_FILE_PATH)
+
+
+if __name__ == "__main__":
+    main()
