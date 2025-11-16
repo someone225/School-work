24f865f42be0fa39e0173dd2beb4dbf4021e57b4
someone225 <marksheng2020@gmail.com>
2025-11-16 03:48:39 -0500

    completed matlab 2 tasks

--- ENGR133/MatLab assignments/MATLAB 2/ma2_team_2_absorb_19.m
+++ ENGR133/MatLab assignments/MATLAB 2/ma2_team_2_absorb_19.m
@@ -1,11 +1,16 @@
+function [out] = ma2_team_2_absorb_19(F2, S, W, chi, alpha, theta)
+
+out = (alpha * W * sin(dtr( theta + chi) ) ) / (S * cos(dtr( theta) ) * (1 - F2 * (1 - alpha)));
+
+end
+
 function out = dtr(in)
 %degrees to radians
 out = in * pi/180;
 end
 
 
-function [out] = absorb(F2, S, W, chi, alpha, theta)
 
-out = (alpha * W * sin(dtr( theta + chi) ) ) / (S * cos(dtr( theta) ) * (1 - F2 * (1 - alpha)));
 
-end
+%test = absorb(0.4393 ,40, 50, 30, 0.5, 18);
+%disp(test);
