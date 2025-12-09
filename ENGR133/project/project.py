fd12d7cb271aa7b8952d9fe9b04133d276735c13
someone225 <marksheng2020@gmail.com>
2025-12-09 07:58:03 -0500

    project main file is somewhat functional now

--- ENGR133/project/project.py
+++ ENGR133/project/project.py
@@ -13,6 +13,9 @@ with open('Weapons.json', 'r') as file:
 with open('reforges.json', 'r') as file:
     reforgeData = json.load(file)
 
+debugMode = 0
+rng = np.random.default_rng()
+
 
 
 
@@ -20,7 +23,8 @@ class player:
     hp = None
     defense = None
     regeneration = None
-    movePriority = None
+    damage = None
+    weapon = None
 
     def __init__(self):
         with open('difficulty.json', 'r') as file:
@@ -29,21 +33,98 @@ class player:
         print("Choose a difficulty:")
         for i in range(0, difficultyData['gameModeData']['dLength']):
             selector = 'difficulty' + str(i + 1)
-            print("(%d) %s: %d starting HP, %d starting DEF, %d HP/turn life regeneration, +%d movement priority" %( (i + 1), difficultyData['gameModeData'][selector]['name'], difficultyData['gameModeData'][selector]['startingHealth'], difficultyData['gameModeData'][selector]['startingDefense'], difficultyData['gameModeData'][selector]['startingRegeneration'], difficultyData['gameModeData'][selector]['startingSpeed'] ) )
+            print("(%d) %s: %d starting HP, %d starting DEF, %d HP/turn life regeneration - \033[93m%s\033[0m" %( (i + 1), difficultyData['gameModeData'][selector]['name'], difficultyData['gameModeData'][selector]['startingHealth'], difficultyData['gameModeData'][selector]['startingDefense'], difficultyData['gameModeData'][selector]['startingRegeneration'], difficultyData['gameModeData'][selector]['description'] ) )
         difficultySelection = 0    
-        difficultySelection = generalUtils.scanNumericalInput('range', difficultySelection, [1, difficultyData['gameModeData']['dLength']], "enter a choice: ")
-        difficultySelection = 'difficulty' + str(difficultySelection)
-        print("you have selected %s mode" %difficultyData['gameModeData'][difficultySelection]['name'])
-        self.hp = difficultyData['gameModeData'][difficultySelection]['startingHealth']
-        self.defense = difficultyData['gameModeData'][difficultySelection]['startingDefense']
-        self.regeneration = difficultyData['gameModeData'][difficultySelection]['startingRegeneration']
-        self.movePriority = difficultyData['gameModeData'][difficultySelection]['startingSpeed']
+        difficultySelection = generalUtils.scanNumericalInput('range', difficultySelection, [0, difficultyData['gameModeData']['dLength']], "enter a choice: ")
+        if difficultySelection == 0:
+            print("Debug mode selected!")
+            debugMode = 1
+        else:
+            difficultySelection = 'difficulty' + str(difficultySelection)
+            print("you have selected %s mode" %difficultyData['gameModeData'][difficultySelection]['name'])
+            self.hp = difficultyData['gameModeData'][difficultySelection]['startingHealth']
+            self.defense = difficultyData['gameModeData'][difficultySelection]['startingDefense']
+            self.regeneration = difficultyData['gameModeData'][difficultySelection]['startingRegeneration']
 
 
 
         
 
-testPlayer = player()
+Player = player()
+Tree = proceduralTree.tree()
+populator = [0] * enemyData['enemyData']['dLength']
+for i in range(0, len(populator)):
+    populator[i] = i + 1
+Tree.setSeed(3, 2, 2, populator, False)
+Tree.populateBuffers()
+
+print("The seed this run is ", end='')
+for i in range(0, len(Tree.seed)):
+    print(Tree.seed[i], sep = '', end = '')
+for i in range(0, len(populator)):
+    print(populator[i], sep = '', end = '')
+
+
+def combat(Player: player, enemy: int):
+    #function to begin a combat encounter
+    enemyName = enemyData['enemyData']['enemy' + str(enemy)]['name']
+    enemyHP = enemyData['enemyData']['enemy' + str(enemy)]['baseHealth']
+    enemyDEF = enemyData['enemyData']['enemy' + str(enemy)]['baseDefense']
+    enemyATK = enemyData['enemyData']['enemy' + str(enemy)]['baseDamage']
+
+    print("Combat initiated against %s - %d HP / %d DEF / %d ATK" % (enemyName, enemyHP, enemyDEF, enemyATK ) ) 
+
+    while enemyHP > 0:
+        moveSelection = 0
+        for turnCount in range(1, 2):
+            match turnCount:
+                case 1:
+                    #player turn
+                    print("Your turn! What will you do?")
+                    print("(1) Attack")
+                    print("(2) Defend")
+                    input = 0
+                    input = generalUtils.scanNumericalInput('range', input, [1,2], 'Make your choice: ')
+                    match input:
+                        case 1:
+                            #attack
+                            moveSelection = 1
+                            print("You dealt %d damage!" %Player.damage)
+                            enemyHP -= Player.damage
+                            print("%s is now on %d HP" % (enemyName, enemyHP) )
+                        case 2:
+                            #defend
+                            moveSelection = 2
+                            print("")
+                case 2:
+                    #enemy turn
+                    print("")
+
+    return 0
+
+def dropLoot():
+    #returns weaponID of dropped weapon, if any, otherwise return 0
+    roll = rng.random()
+    for i in range(weaponData['weaponData']['dLength'], 0, -1):
+        if(roll <= weaponData['weaponData']['weapon' + str(i)]['obtainRate']):
+            return i
+    return 0
+    
+
+#the current buffer will indicate which enemies are to be fought on the current depth
+#the future buffer will indicate which enemies are to be fought on the next depth
+
+if(debugMode == 1):
+    print("\ndebug: enemies encountered on current depth: ", end = '')
+    for i in range(0, len(Tree.presentBuffer)):
+        selector = 'enemy' + str(Tree.presentBuffer[i])
+        print(enemyData['enemyData'][selector]['name'], sep = '', end = '')
+        if i < len(Tree.presentBuffer) - 1:
+            print(", ", sep = '', end = '')
+    print("\n")
+
+
+
 
 
 
